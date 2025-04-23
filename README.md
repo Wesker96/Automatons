9a0b5f858a6eadfd55c4f7a3ee2019638adfe8c8
HammerMeow <ps.shishkov1@gmail.com>
2025-04-23 20:00:24 +0300

    - add template generator for testbench

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Automatons
 Automatons for Sublime Text 4
 
+Dependencies:
+ - [Plugin: Verilog](https://packagecontrol.io/packages/Verilog)
+
+
 Clone repo to 'C:\Users\user\AppData\Roaming\Sublime Text\Packages'
+
+
+
--- automatons.py
+++ automatons.py
@@ -3,7 +3,7 @@ import os
 import sublime
 import sublime_plugin
 
-from .lib.GenTemplate import SrcTemplate
+from .lib.GenTemplate import SrcTemplate, TbTemplate
 
 
 class SrcTemplateCommand(sublime_plugin.TextCommand):
@@ -17,6 +17,10 @@ class SrcTemplateCommand(sublime_plugin.TextCommand):
 
 
 class TbTemplateCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.sel().clear()
-        self.view.sel().add_all(self.view.find_all("example"))
+    def run(self, edit, syntax):
+        obj_test = TbTemplate()
+
+        cursor_position = self.view.sel()[0].begin()
+        self.view.insert(edit, cursor_position, obj_test.insert())
+
+        self.view.set_syntax_file(syntax)
